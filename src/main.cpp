105a1ced8e0f1493a4d2f3832cfb433cefcfa0f8
teostereciu <teostereciu@gmail.com>
2023-03-01 00:36:33 +0100

    basic converter for eur, usd, ron

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,41 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    cout << "Hello, World!\n";
+
+    double fromVal, toVal;
+    string fromCurr, toCurr;
+    cout << "From value: ";
+    cin >> fromVal;
+    do {
+        cout << "\nFrom currency (EUR, USD, RON): ";
+        cin >> fromCurr;
+    } while (fromCurr != "EUR" && fromCurr != "USD" && fromCurr != "RON");
+    
+    do {
+        cout << "\nTo currency (EUR, USD, RON): ";
+        cin >> toCurr;
+    } while (fromCurr != "EUR" && fromCurr != "USD" && fromCurr != "RON");
+    
+    //double convRates[3][3] = {{1,1.06,4.93},{0.95,1,4.66},{0.2,0.21,1}};
+    double convRates[3][3] = {{1,1.06,4.93},{0.95,1,4.66},{0.2,0.21,1}};
+    int from=0, to=0;
+    if (fromCurr == "EUR") {
+        from = 0;
+    } else if (fromCurr == "USD") {
+        from = 1;
+    } else {
+        from = 2;
+    }
+
+    if (toCurr == "EUR") {
+        from = 0;
+    } else if (fromCurr == "USD") {
+        from = 1;
+    } else {
+        from = 2;
+    }
+    
+    toVal = fromVal * convRates[from][to];
+    cout << fromVal << " " << fromCurr << " is " << toVal << " " << toCurr << ".\n";
     return 0;
 }
